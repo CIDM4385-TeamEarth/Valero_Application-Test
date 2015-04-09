4190079a60c62cf6b904026c1f5f3d54dd8161cb
Nhat Doan <dnhat12@yahoo.com>
2015-04-09 17:47:53 -0500

    Updated menu for every view - Nhat, Ezau, & Travis

--- app/controllers/nearbyLocations.js
+++ app/controllers/nearbyLocations.js
@@ -1,2 +1,14 @@
-var args = arguments[0] || {};
+function index(e){
+	var index = Alloy.createController("index").getView();
+	index.open();
+}
 
+function storeLocations(e){
+	var storeLocations = Alloy.createController("storeLocations").getView();
+	storeLocations.open();
+}
+
+function tripPlanner(e){
+	var tripPlanner = Alloy.createController("tripPlanner").getView();
+	tripPlanner.open();
+}
